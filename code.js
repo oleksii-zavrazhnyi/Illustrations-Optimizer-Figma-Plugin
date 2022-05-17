f80a8dc72c5782107432d655d3babad8443da07f
Oleksii Zavrazhnyi <alex.zavrazhniy@gmail.com>
2022-05-17 22:59:32 +0300

    release 1.0

---
+++ code.js
@@ -0,0 +1,32 @@
+figma.showUI(__html__);
+const allowedNodes = ['FRAME', 'COMPONENT'];
+figma.ui.onmessage = msg => {
+    if (msg.type === 'fill') {
+        for (const node of figma.currentPage.selection) {
+            if (allowedNodes.includes(node.type)) {
+                node.exportAsync({ format: 'PNG', constraint: { type: 'SCALE', value: msg.retina ? 2 : 1 } })
+                    .then(res => {
+                    node.fills = [{ type: "IMAGE", scaleMode: msg.fillScale, imageHash: figma.createImage(res).hash }];
+                    if ('children' in node) {
+                        for (const child of node.children) {
+                            child.visible = false;
+                        }
+                    }
+                })
+                    .catch(err => console.error(err));
+            }
+        }
+    }
+    if (msg.type === 'unfill') {
+        for (const node of figma.currentPage.selection) {
+            if (allowedNodes.includes(node.type)) {
+                node.fills = [];
+                if ('children' in node) {
+                    for (const child of node.children) {
+                        child.visible = true;
+                    }
+                }
+            }
+        }
+    }
+};
