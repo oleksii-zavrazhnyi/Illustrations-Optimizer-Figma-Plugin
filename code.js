6fafc5fd5bbfee0b782193183e3f18da3f045302
Oleksii Zavrazhnyi <alex.zavrazhniy@gmail.com>
2023-01-13 14:34:35 +0200

    New UI More scales to select Error reporting

--- code.js
+++ code.js
@@ -2,9 +2,14 @@ figma.showUI(__html__);
 const allowedNodes = ['FRAME', 'COMPONENT'];
 figma.ui.onmessage = msg => {
     if (msg.type === 'optimize') {
+        let current = 0;
+        figma.ui.postMessage({
+            function: "start",
+            total: figma.currentPage.selection.length
+        });
         for (const node of figma.currentPage.selection) {
             if (allowedNodes.includes(node.type)) {
-                node.exportAsync({ format: 'PNG', constraint: { type: 'SCALE', value: msg.retina ? 2 : 1 } })
+                node.exportAsync({ format: 'PNG', constraint: { type: 'SCALE', value: msg.scale } })
                     .then(res => {
                     node.fills = [{ type: "IMAGE", scaleMode: msg.fillScale, imageHash: figma.createImage(res).hash }];
                     if ('children' in node) {
@@ -12,12 +17,16 @@ figma.ui.onmessage = msg => {
                             child.visible = false;
                         }
                     }
-                })
-                    .catch(err => console.error(err));
+                    figma.ui.postMessage({
+                        function: "progress",
+                        current: ++current
+                    });
+                }).catch(err => figma.notify("Unable to optimize. Error " + err.message, { error: true }));
             }
         }
     }
     if (msg.type === 'revert') {
+        figma.ui.postMessage({ function: "start" });
         for (const node of figma.currentPage.selection) {
             if (allowedNodes.includes(node.type)) {
                 node.fills = [];
